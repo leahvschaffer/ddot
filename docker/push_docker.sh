7f092e85374bc927804138ce745bea605702fa44
Chris Churas <churas.camera@gmail.com>
2021-08-18 11:14:29 -0700

    going to remove for now since this will be a python package

--- docker/push_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-version="v1.0"
-
-docker push michaelkyu/ddot-anaconda2:$version
-docker push michaelkyu/ddot-anaconda3:$version
-
-docker push michaelkyu/ddot-anaconda2:latest
-docker push michaelkyu/ddot-anaconda3:latest
